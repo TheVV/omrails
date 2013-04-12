8593e1c0fcae1395e3af98e1dbda4a9b341782a5
James Kelly <jk@vagabondvanguard.com>
2013-04-12 14:47:55 +0100

    Reroute root to pins#index

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Omrails::Application.routes.draw do
 
   get 'about' => 'pages#about'
 
-  root :to => 'pins#index'
-
+ root :to => 'pins#index'
+ 
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
